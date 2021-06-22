0ff2c86276ef77508327c12693e52480e2a6b465
ogaki0612 <kog.v.0612@gmail.com>
2021-06-22 09:38:38 +0900

    マイグレーション

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,6 +1,4 @@
 class LogsController < ApplicationController
-  def index
-    
-  end
+
 
 end
--- db/migrate/20210613232527_create_logs.rb
+++ db/migrate/20210613232527_create_logs.rb
@@ -3,6 +3,8 @@ class CreateLogs < ActiveRecord::Migration[6.0]
     create_table :logs do |t|
 
       t.timestamps
+      t.string :title, null: false
+      t.references :aqua, foreign_key: true
     end
   end
 end
